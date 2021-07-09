51d8621062d095fe29cf9551a817b44544ce7fba
nhoanglong20 <hoanglongmcs1@gmail.com>
2021-07-09 16:35:56 +0700

    create new subject API

--- src/controllers/subject-controller.js
+++ src/controllers/subject-controller.js
@@ -4,13 +4,17 @@ const AppError = require('./../utils/appError');
 const subjectServ = require('../services/subject-service');
 
 exports.getAll = catchAsync(async (req, res, next) => {
-    let filter = {};
-    if (req.query.limit) filter = { ...filter, limit: req.query.limit };
-    const records = await subjectServ.getAll(filter);
+    const { limit, offset } = req.query;
+    const filter = {};
+    filter.limit =
+        (Number.parseInt(limit) && Number.parseInt(limit) >= 0) || null;
+    filter.offset = offset || 0;
 
+    const records = await subjectServ.getAll(filter);
     if (!records || records.length === 0) {
         return next(new AppError('No record found', 404));
     }
+
     res.status(200).json({
         status: 'success',
         data: {
@@ -22,9 +26,11 @@ exports.getAll = catchAsync(async (req, res, next) => {
 exports.getOne = catchAsync(async (req, res, next) => {
     const { subject_id } = req.params;
     const record = await subjectServ.getOne(subject_id);
+
     if (!record || record.length === 0) {
         return next(new AppError('No record found with that id', 404));
     }
+
     res.status(200).json({
         status: 'success',
         data: {
@@ -34,5 +40,18 @@ exports.getOne = catchAsync(async (req, res, next) => {
 });
 
 exports.createOne = catchAsync(async (req, res, next) => {
-  
+    const { subject_id, prerequisite_subject, name, credits } = req.body;
+    const subject = { subject_id, prerequisite_subject, name, credits };
+
+    const record = await subjectServ.createOne(subject);
+
+    if (!record || record.length === 0) {
+        return next(new AppError('Some thing went wrong when creating new subject, try again', 400));
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            subject: record
+        }
+    });
 });
--- src/services/subject-service.js
+++ src/services/subject-service.js
@@ -2,13 +2,25 @@ const AppError = require('../utils/appError');
 const pool = require('../database');
 
 exports.getAll = async filter => {
-    let sql = `SELECT * from subjects LIMIT $1`;
-    const records = await pool.query(sql, [filter.limit]);
+    let sql = `SELECT * from subjects LIMIT $1 OFFSET $2`;
+    const records = await pool.query(sql, [filter.limit, filter.offset]);
     return records.rows;
 };
 
-exports.getOne = async (_id) => {
+exports.getOne = async _id => {
     const sql = `SELECT * from subjects WHERE subject_id = $1`;
     const records = await pool.query(sql, [_id]);
+    return records.rows[0];
+};
+
+exports.createOne = async subject => {
+    const sql = `INSERT INTO subjects (subject_id, prerequisite_subject, name, credits) 
+                    VALUES ($1, $2, $3, $4) RETURNING *`;
+    const records = await pool.query(sql, [
+        subject.subject_id,
+        subject.prerequisite_subject,
+        subject.name,
+        subject.credits
+    ]);
     return records.rows[0];
 };
