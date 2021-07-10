0d307cd2de4f40224f0b3a524945ce9b20e580c0
nhoanglong20 <hoanglongmcs1@gmail.com>
2021-07-10 17:27:19 +0700

    add protect route to subject route

--- src/routes/subject-route.js
+++ src/routes/subject-route.js
@@ -1,12 +1,25 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
 const subjectCtrl = require('../controllers/subject-controller');
+const authCtrl = require('../controllers/auth-controller');
 
-router.post('/', subjectCtrl.createOne);
-router.get('/:subject_id', subjectCtrl.getOne);
+const router = express.Router();
 
-router.patch('/:subject_id', subjectCtrl.updateOne);
-router.route('/').get(subjectCtrl.getAll);
+router
+    .route('/')
+    .get(subjectCtrl.getAll)
+    .post(
+        authCtrl.protect,
+        authCtrl.restrictTo('admin', 'morderator'),
+        subjectCtrl.createOne
+    );
+    
+router
+    .route('/:subject_id')
+    .get('/:subject_id', subjectCtrl.getOne)
+    .patch(
+        authCtrl.protect,
+        authCtrl.restrictTo('admin', 'morderator'),
+        subjectCtrl.updateOne
+    );
 
 module.exports = router;
